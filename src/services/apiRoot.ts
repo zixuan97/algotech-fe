2b8ffa779e62940c90901acccf018bdd58c3d6ac
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-08-31 23:31:14 +0800

    fix: frontend prefix deployment issue

--- src/services/apiRoot.ts
+++ src/services/apiRoot.ts
@@ -1,5 +1,5 @@
 const LOCAL_API_ROOT = 'http://localhost:4000';
 
-const apiRoot = process.env.API_ROOT || LOCAL_API_ROOT;
+const apiRoot = process.env.REACR_APP_API_ROOT || LOCAL_API_ROOT;
 
 export default apiRoot;
