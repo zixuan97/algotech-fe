17650af90630f069e7e152b397f09ba3f273ad34
Kelly <ngkelly@u.nus.edu>
2022-09-12 23:05:27 +0800

    Category CRUD (but not important for now)

---
+++ src/components/inventory/CategoryCellAction.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import { GridRenderCellParams } from '@mui/x-data-grid';
+import '../../styles/common/actionCells.scss';
+import { useNavigate } from 'react-router';
+import { createSearchParams } from 'react-router-dom';
+
+const CategoryCellAction = ({ id }: GridRenderCellParams) => {
+  const navigate = useNavigate();
+  return (
+      <div className='action-cell'>
+        <Button
+          variant='contained'
+          onClick={() =>
+            navigate({
+              pathname: '/inventory/categoryDetails',
+              search: createSearchParams({
+                id: id.toString()
+              }).toString()
+            })
+          }
+          >
+            View Category
+          </Button>
+          {}
+      </div>
+  );
+};
+
+export default CategoryCellAction;
