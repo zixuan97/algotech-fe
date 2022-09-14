d483d58155bad22f14341797d82f491a757c658f
zixuan97 <ngzixuan1997@hotmail.com>
2022-09-14 18:56:10 +0800

    Refactor and restyle ViewAccount page

--- src/pages/account/ViewAccount.tsx
+++ src/pages/account/ViewAccount.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { Box, Paper, Button, IconButton, Tooltip, Grid, CircularProgress } from '@mui/material';
+import { Box, Paper, Button, IconButton, Tooltip, Grid, CircularProgress, Typography, Alert, TextField, MenuItem } from '@mui/material';
 import '../../styles/pages/accounts.scss';
 import { ChevronLeft } from '@mui/icons-material';
 import {
   deleteUserSvc,
   disableUserSvc,
+  editUserSvc,
   enableUserSvc,
   getUserDetailsSvc
 } from 'src/services/accountService';
 import asyncFetchCallback from '../../../src/services/util/asyncFetchCallback';
 import { User } from 'src/models/types';
 import ConfirmationModal from 'src/components/common/ConfirmationModal';
-import { toast } from 'react-toastify';
-import { Typography } from '@mui/material';
+import { AlertType } from 'src/components/common/Alert';
+import { roles } from 'src/components/account/accountTypes';
 interface ModalProps {
   wrapperParam: wrapperParam,
   modalOpen: boolean,
@@ -37,17 +38,32 @@ const WrapperModal = ({ wrapperParam, modalOpen, onClose }: ModalProps) => {
   )
 };
 
+const placeholderUser: User = {
+  //note: id is temp holder, BE doesn't consume id on create
+  id: 0,
+  email: '',
+  role: '',
+  status: '',
+  first_name: '',
+  last_name: '',
+  password: '',
+  isVerified: true
+};
+
 const ViewAccount = () => {
   let params = new URLSearchParams(window.location.search);
   const navigate = useNavigate();
-  const [user, setUser] = useState<User>();
+  const [user, setUser] = useState<User>(placeholderUser);
   const [modalOpen, setModalOpen] = useState<boolean>(true);
   const [loading, setLoading] = React.useState<boolean>(true);
+  const [alert, setAlert] = useState<AlertType | null>(null);
+  const [edit, setEdit] = useState<boolean>(false);
   const [wrapParam, setWrapParam] = useState<wrapperParam>({
     title: '',
     body: '',
     funct: () => { }
   })
+
   const loaded = useRef(false);
   const id = params.get('id');
 
@@ -80,17 +96,17 @@ const ViewAccount = () => {
       asyncFetchCallback(
         deleteUserSvc(id),
         () => {
-          toast.success('Account deleted.', {
-            position: 'top-right',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined
+          setAlert({
+            severity: 'success',
+            message: 'Account deleted.'
           });
           setModalOpen(false);
           navigate('/accounts');
+        }, () => {
+          setAlert({
+            severity: 'error',
+            message: 'Cannot delete user at this point. Try again later.'
+          });
         }
       );
     setModalOpen(false);
@@ -101,14 +117,9 @@ const ViewAccount = () => {
       asyncFetchCallback(
         disableUserSvc(id),
         () => {
-          toast.warning('Account disabled.', {
-            position: 'top-right',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined
+          setAlert({
+            severity: 'success',
+            message: 'Account disabled.'
           });
           setModalOpen(false);
           navigate(`/accounts/viewAccount?id=${id}`);
@@ -122,14 +133,9 @@ const ViewAccount = () => {
       asyncFetchCallback(
         enableUserSvc(id),
         () => {
-          toast.success('Account enabled.', {
-            position: 'top-right',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined
+          setAlert({
+            severity: 'success',
+            message: 'Account enabled.'
           });
           setModalOpen(false);
           navigate(`/accounts/viewAccount?id=${id}`);
@@ -160,6 +166,40 @@ const ViewAccount = () => {
       );
   }, [user]);
 
+  const userFieldOnChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    key: string
+  ) => {
+    setUser((paramUser: User) => {
+      return {
+        ...paramUser,
+        [key]: event.target.value
+      };
+    });
+  };
+
+  const handleSaveButtonClick = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    asyncFetchCallback(
+      editUserSvc(user),
+      () => {
+        setAlert({
+          severity: 'success',
+          message: 'Account edited.'
+        });
+        setLoading(false);
+      },
+      () => {
+        setAlert({
+          severity: 'error',
+          message: 'Error saving changes for account! Try again later.'
+        });
+        setLoading(false);
+      }
+    );
+  };
+
   return (
     <>
       <WrapperModal
@@ -182,17 +222,21 @@ const ViewAccount = () => {
             <h1>View User Account </h1>
             <div className='button-group'>
               {loading && <CircularProgress color='secondary' />}
-              <Link
-                to="/accounts/editAccount"
-                state={user}
+              <Button
+                variant='contained'
+                className='create-btn'
+                color='primary'
+                onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                  if (!edit) {
+                    setEdit(true);
+                  } else {
+                    handleSaveButtonClick(e);
+                    setEdit(false);
+                  }
+                }}
               >
-                <Button
-                  variant='contained'
-                  className='create-btn'
-                  color='primary'>
-                  EDIT
-                </Button>
-              </Link>
+                EDIT
+              </Button>
               <Button
                 type='submit'
                 variant='contained'
@@ -213,42 +257,112 @@ const ViewAccount = () => {
               </Button>
             </div>
           </div>
-
+          {alert && (
+            <Alert severity={alert.severity} onClose={() => setAlert(null)} style={{ margin: '1%' }}>
+              {alert.message}
+            </Alert>
+          )}
           <Paper elevation={2}>
             <div className='content-body'>
               <div className='right-content'>
                 <Grid container spacing={1}>
                   <Grid item xs={6}>
-                    <div>
-                      <h4>First Name</h4>
-                      <Typography>
-                        {user?.first_name}
-                      </Typography>
-                    </div>
+                    {edit ? (
+                      <TextField
+                        fullWidth
+                        required
+                        id='outlined-quantity'
+                        label='First Name'
+                        name='firstName'
+                        placeholder='eg.: John'
+                        value={user?.first_name}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                          userFieldOnChange(e, 'first_name')
+                        }
+                      />
+                    ) : (
+                      <div>
+                        <h4>First Name</h4>
+                        <Typography>
+                          {user?.first_name}
+                        </Typography>
+                      </div>
+                    )}
                   </Grid>
+
                   <Grid item xs={6}>
-                    <div>
-                      <h4>Last Name</h4>
-                      <Typography>
-                        {user?.last_name}
-                      </Typography>
-                    </div>
+                    {edit ? (
+                      <TextField
+                        fullWidth
+                        required
+                        id='outlined-quantity'
+                        label='Last Name'
+                        name='lastName'
+                        placeholder='eg.: Tan'
+                        value={user?.last_name}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                          userFieldOnChange(e, 'last_name')
+                        }
+                      />
+                    ) : (
+                      <div>
+                        <h4>Last Name</h4>
+                        <Typography>
+                          {user?.last_name}
+                        </Typography>
+                      </div>
+                    )}
                   </Grid>
-                  <Grid item xs={6}>
-                    <div>
-                      <h4>Email</h4>
-                      <Typography>
-                        {user?.email}
-                      </Typography>
-                    </div>
+                  <Grid item xs={edit ? 12 : 6}>
+                    {edit ? (
+                      <TextField
+                        required
+                        fullWidth
+                        id='outlined-quantity'
+                        label='Email'
+                        name='email'
+                        placeholder='eg.: johntan@gmail.com'
+                        value={user?.email}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                          userFieldOnChange(e, 'email')
+                        }
+                      />
+                    ) : (
+                      <div>
+                        <h4>Email</h4>
+                        <Typography>
+                          {user?.email}
+                        </Typography>
+                      </div>
+                    )}
                   </Grid>
-                  <Grid item xs={6}>
-                    <div>
-                      <h4>Role</h4>
-                      <Typography>
-                        {user?.role}
-                      </Typography>
-                    </div>
+                  <Grid item xs={edit ? 12 : 6}>
+                    {edit ? (
+                      <TextField
+                        required
+                        fullWidth
+                        id='outlined-field'
+                        select
+                        label='Role'
+                        value={user?.role}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                          userFieldOnChange(e, 'role')
+                        }
+                      >
+                        {roles.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    ) : (
+                      <div>
+                        <h4>Role</h4>
+                        <Typography>
+                          {user?.role}
+                        </Typography>
+                      </div>
+                    )}
                   </Grid>
                 </Grid>
               </div>
