97e2e368cafd704bcd79a6af649f80d054314049
zixuan97 <ngzixuan1997@hotmail.com>
2022-09-16 18:16:57 +0800

    SR1 Fixes (2)

--- src/pages/account/ViewAccount.tsx
+++ src/pages/account/ViewAccount.tsx
@@ -22,7 +22,7 @@ import {
   getUserDetailsSvc
 } from 'src/services/accountService';
 import asyncFetchCallback from '../../../src/services/util/asyncFetchCallback';
-import { User } from 'src/models/types';
+import { User, UserRole } from 'src/models/types';
 import ConfirmationModal from 'src/components/common/ConfirmationModal';
 import TimeoutAlert, { AlertType } from '../../components/common/TimeoutAlert';
 import { roles } from 'src/components/account/accountTypes';
@@ -249,7 +249,7 @@ const ViewAccount = () => {
                 variant='contained'
                 className='create-btn'
                 color='primary'
-                disabled={!validator.isEmail(user.email) || validator.isEmpty(user?.last_name) || validator.isEmpty(user?.first_name)}
+                disabled={edit && (!validator.isEmail(user.email) || validator.isEmpty(user?.last_name) || validator.isEmpty(user?.first_name))}
                 onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
                   if (!edit) {
                     setEdit(true);
