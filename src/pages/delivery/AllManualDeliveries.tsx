38d8b6d2b14cf9c43a99814dab49a7dba297db09
Weng Jia Jun <e0406810@u.nus.edu>
2022-09-23 09:59:00 +0800

    delivery

--- src/pages/delivery/AllManualDeliveries.tsx
+++ src/pages/delivery/AllManualDeliveries.tsx
@@ -3,12 +3,14 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import DeliveryCellAction from 'src/components/delivery/DeliveryCellAction';
 import '../../styles/pages/inventory/inventory.scss';
 import '../../styles/common/common.scss';
+import '../../styles/pages/delivery/map.scss';
 import { Button, TextField } from '@mui/material';
 import { Search } from '@mui/icons-material';
 import { DeliveryOrder } from '../../models/types';
 import asyncFetchCallback from 'src/services/util/asyncFetchCallback';
 import { getAllDeliveries } from 'src/services/deliveryServices';
 import { useNavigate } from 'react-router';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'Delivery ID', flex: 1 },
@@ -69,6 +71,18 @@ const AllManualDeliveries = () => {
   return (
     <div className='delivery'>
       <h1>All Manual Deliveries</h1>
+      <div> 
+          {/* var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
+            detectRetina: true,
+            maxZoom: 18,
+            minZoom: 11,
+            //Do not remove this attribution
+            //attribution: '<img src="https://www.onemap.gov.sg/docs/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
+          }); */}
+          <MapContainer>
+            <TileLayer url = "https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png"/>
+          </MapContainer>
+      </div>
       <div className='grid-toolbar'>
         <div className='search-bar'>
           <Search />
