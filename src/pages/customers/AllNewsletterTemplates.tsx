1d1405be41779fb40788af18a69c3017aafb6faf
destineeow <destineeow32@gmail.com>
2022-10-21 11:41:57 +0800

    add search for newsletter templates

--- src/pages/customers/AllNewsletterTemplates.tsx
+++ src/pages/customers/AllNewsletterTemplates.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import '../../styles/pages/customer/customer.scss';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { getAllNewsletterTemplates } from 'src/services/customerService';
 import { NewsletterTemplate } from 'src/models/types';
 import asyncFetchCallback from 'src/services/util/asyncFetchCallback';
 import ViewNewsletterButton from 'src/components/customers/ViewNewsletterButton';
+import { Search } from '@mui/icons-material';
 
 const columns: GridColDef[] = [
   { field: 'name', headerName: 'Newsletter Name', flex: 1 },
@@ -27,6 +28,9 @@ const NewsletterTemplates = () => {
   const [newsletterTemplates, setNewsletterTemplates] = React.useState<
     NewsletterTemplate[]
   >([]);
+  const [filteredNewsletterTemplates, setFilteredNewsletterTemplates] =
+    React.useState<NewsletterTemplate[]>([]);
+  const [searchField, setSearchField] = React.useState<string>('');
   const [loading, setLoading] = React.useState<boolean>(false);
 
   React.useEffect(() => {
@@ -41,25 +45,54 @@ const NewsletterTemplates = () => {
     );
   }, []);
 
+  React.useEffect(() => {
+    setFilteredNewsletterTemplates(
+      searchField
+        ? newsletterTemplates.filter((template) =>
+            Object.values(template).some((value) =>
+              String(value).toLowerCase().includes(searchField.toLowerCase())
+            )
+          )
+        : newsletterTemplates
+    );
+  }, [searchField, newsletterTemplates]);
+
+  const handleSearchFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchField(e.target.value);
+  };
+
   return (
     <div className='newsletter-templates'>
       <h1>Newsletter Templates</h1>
       <div className='newsletter-templates-toolbar'>
-        <Button
-          variant='contained'
-          size='medium'
-          sx={{ height: 'fit-content' }}
-          onClick={() =>
-            navigate({ pathname: '/customer/createNewsletterTemplate' })
-          }
-        >
-          Create Newsletter Template
-        </Button>
+        <div className='search-bar'>
+          <Search />
+          <TextField
+            id='search'
+            label='Search'
+            margin='normal'
+            placeholder='Newsletter Name, Email Subject or Discount Code'
+            fullWidth
+            onChange={handleSearchFieldChange}
+          />
+        </div>
+        <div className='newsletter-templates-button-container'>
+          <Button
+            variant='contained'
+            size='medium'
+            sx={{ height: 'fit-content' }}
+            onClick={() =>
+              navigate({ pathname: '/customer/createNewsletterTemplate' })
+            }
+          >
+            Create Newsletter Template
+          </Button>
+        </div>
       </div>
       <DataGrid
         columns={columns}
         loading={loading}
-        rows={newsletterTemplates}
+        rows={filteredNewsletterTemplates}
         autoHeight
       />
     </div>
