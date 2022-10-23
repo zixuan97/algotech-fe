7a851513ae84346c2b4d6c1dc37c165e29fd95f8
jj-weng <e0406810@u.nus.edu>
2022-10-23 14:12:49 +0800

    fixed chip's colour

--- src/components/customers/PaymentModeCell.tsx
+++ src/components/customers/PaymentModeCell.tsx
@@ -9,20 +9,14 @@ const PaymentModeCell = ({ row }: GridRenderCellParams) => {
   return paymentMode === PaymentMode.CREDIT_CARD? (
     <Chip
       label='Credit Card'
-      style={{ backgroundColor: '#F6B992', fontFamily: 'Poppins' }}
+      style={{ backgroundColor: 'FBE9BA', fontFamily: 'Poppins' }}
     />
-  ) : paymentMode === PaymentMode.BANK_TRANSFER ? (
-    <Chip
-      label='Bank Transfer'
-      style={{ backgroundColor: '#9BBFE0', fontFamily: 'Poppins' }}
-    />
-  ) :  (
+  )  :  (
     <Chip
       label='Paynow'
       style={{
-        backgroundColor: '#C6D68F',
-        fontFamily: 'Poppins',
-        color: 'white'
+        backgroundColor: '#E2D8F5',
+        fontFamily: 'Poppins'
       }}
     />
   );
